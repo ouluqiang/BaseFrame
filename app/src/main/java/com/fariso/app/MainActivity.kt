c784c24e6906fc7807a11062a1f1b32b738aa52e
ouluqiang <632521056@qq.com>
2021-10-28 14:49:26 +0800

    提交

--- app/src/main/java/com/fariso/app/MainActivity.kt
+++ app/src/main/java/com/fariso/app/MainActivity.kt
@@ -1,12 +1,13 @@
-package com.fariso.composebase
+package com.fariso.app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.olq.baseframe.AppUtils
 
-class TestActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_test)
-//        AppU
+        setContentView(R.layout.activity_main)
+//        AppUtils.getAppSHA(this)
     }
 }
--- app/src/test/java/com/fariso/app/ExampleUnitTest.kt
+++ app/src/test/java/com/fariso/app/ExampleUnitTest.kt
@@ -1,4 +1,4 @@
-package com.fariso.composebase
+package com.fariso.app
 
 import org.junit.Test
 
